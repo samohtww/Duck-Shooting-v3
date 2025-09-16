b9b697595abfa0eee6358c339f6d4a2c8dff3896
samohtww <32458561+samohtww@users.noreply.github.com>
2025-09-16 16:31:46 +0200

    Proof of concept delayed shooting including the sound cue

--- Screens/play.py
+++ Screens/play.py
@@ -3,13 +3,17 @@ import Screens.menu as menu
 from Assets.PythonFiles.font import get_font
 from Assets.PythonFiles.button import Button
 
+pygame.mixer.init()
+ping_sound = pygame.mixer.Sound("Assets/sounds/ping.mp3")
+
 def play(screen, back_ground, settings):
     screen.fill("black")
     pygame.display.update()
     clock = pygame.time.Clock()
     timer = sys.maxsize
     dt = 0
-    color = ""
+    modus = ""
+    maxInterval = 3
 
     while True:
         options_text = get_font(45).render("Play", True, "white")
@@ -27,27 +31,33 @@ def play(screen, back_ground, settings):
                 menu.menu(screen, back_ground, settings)
             if keys[pygame.K_DOWN]:
                 timer = 5
+                modus = "Prepare"
+                interval = 1
 
-        if timer <= 0 and color == "":  # When the time is up ...
-            color = "green"
-            screen.fill("green")
-            pygame.display.update()
+        if timer <= 0 and modus == "Prepare" and interval <= maxInterval:
+            roundtext = "Ronde " + str(interval)
+            pygame.mixer.Sound.play(ping_sound)
             timer = 5
-        if timer <= 0 and color == "green":  # When the time is up ...
-            color = "blue"
-            screen.fill("blue")
-            pygame.display.update()
-            timer = 5
-        if timer <= 0 and color == "blue":  # When the time is up ...
-            color = "red"
+            modus = "Shooting"
+            
+            # Dit is om te laten zien dat het werkt, zodra er logica aan toegevoegd wordt kan dit weg
             screen.fill("red")
             pygame.display.update()
+
+            # Dit moet na de update() anders wordt het namelijk meteen weer weggehaald
+            round_text = get_font(45).render(roundtext, True, "white")
+            round_rect = round_text.get_rect(center=(200, 35))
+            screen.blit(round_text, round_rect)
+        if timer <= 0 and modus == "Shooting":
             timer = 5
-        if timer <= 0 and color == "red":  # When the time is up ...
-            color = "purple"
-            screen.fill("purple")
+            interval += 1
+            modus = "Prepare"
+            
+            # Dit is om te laten zien dat het werkt, zodra er logica aan toegevoegd wordt kan dit weg
+            screen.fill("green")
             pygame.display.update()
-            timer = sys.maxsize
+
+
 
         dt = clock.tick(60) / 1000
 
